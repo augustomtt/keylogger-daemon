7beac55804742c376172e8ff21ed100036ef0b0f
mateoetchepare <mateoetchepare@live.com.ar>
2024-04-09 16:12:58 -0300

    added server and deleted some README files

---
+++ server/index.mjs
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+
+const app = express();
+
+app.use(bodyParser.text({type: '*/*'}));
+
+const ip = 'localhost';
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`server working on port ${PORT}`);
+})
+
+app.post('/api/file', (request, response) => {
+    const text = request.body;
+    console.log(text);
+    try {
+        fs.writeFileSync('userData.txt', text);
+        response.status(200);
+    } catch (err) {
+        console.error(err)
+        response.status(500);
+    }
+});
